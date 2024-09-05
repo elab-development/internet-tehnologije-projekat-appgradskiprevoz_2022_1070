4d9ef1c48d5e9022b28a65bd60d01e4c855218fb
NikolaITEH <ns20221070@student.fon.bg.ac.rs>
2024-09-05 19:26:16 +0200

    custom snippet

--- frontend/src/components/useSearchSort.js
+++ frontend/src/components/useSearchSort.js
@@ -6,7 +6,7 @@ import { useState} from "react";
 function useSearchSort() {
     const[search, setSearch]=useState();
     const[sortBy, setSortBy]=useState();
-    const[sortOrder, setSortOrder]=useState();
+    const[sortOrder, setSortOrder]=useState('asc');
 
     return{
         search,
